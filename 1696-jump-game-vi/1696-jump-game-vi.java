c017d932b24f181808502620b12ed373e6fb4cfe
reach2nimit <reach2nimit@gmail.com>
2026-04-14 11:00:08 +0530

    Time: 23 ms (87.43%), Space: 81.3 MB (82.51%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        int len = nums.length;
+        int[] score = new int[len];
+        score[0] = nums[0];
+        Deque<Integer> dq = new ArrayDeque();
+        dq.add(0);
+
+        for(int i = 1; i<len; i++){
+            while(!dq.isEmpty() && dq.peekFirst() + k < i)
+                dq.pollFirst();
+            
+            score[i] = score[dq.peekFirst()] + nums[i];
+
+            while(!dq.isEmpty() && score[i]>=score[dq.peekLast()])
+                dq.pollLast();
+            
+            dq.add(i);
+        }
+        return score[len-1];
+    }
+}
