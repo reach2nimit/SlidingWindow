689f50cb3363ad8703f99cb8a241eff93be584a3
reach2nimit <reach2nimit@gmail.com>
2026-04-17 14:49:16 +0530

    Time: 20 ms (96.45%), Space: 52 MB (86.88%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -0,0 +1,25 @@
+class Solution {
+    
+    public int sumSubarrayMins(int[] arr) {
+        final int MOD = 1000000007;
+        Deque<Integer> st = new ArrayDeque();
+        long result = 0;
+
+            //imp comparing condition
+        for(int i = 0; i <= arr.length; i++){
+
+            while(!st.isEmpty() && (i == arr.length || arr[st.peek()] >= arr[i])){
+
+                int mid = st.pop();
+                int left = (st.isEmpty()) ? -1 : st.peek();
+                int right = i;
+
+                long count = (mid - left) * (right - mid) % MOD;
+                result+=(count * arr[mid])%MOD;
+                result%=MOD;
+            }
+            st.push(i);
+        }
+        return (int)result;
+    }
+}
