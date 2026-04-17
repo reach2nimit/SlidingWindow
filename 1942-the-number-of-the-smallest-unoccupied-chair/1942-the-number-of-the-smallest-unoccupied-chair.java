528b476f8ff402437d4686d6f081a4e1cc2ad433
reach2nimit <reach2nimit@gmail.com>
2026-04-17 09:39:24 +0530

    Time: 34 ms (93.51%), Space: 51.2 MB (77.48%) - LeetHub

---
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int smallestChair(int[][] times, int targetFriend) {
+        
+        int targetStart = times[targetFriend][0];
+
+        Arrays.sort(times, (a,b) -> a[0] - b[0]);
+
+        Queue<Integer> availableChairs = new PriorityQueue<>();
+        Queue<int[]> leaveChair = new PriorityQueue<>(
+            (a,b) -> a[0] - b[0]
+        );
+
+        int currChair = 0;
+
+        for(int[] time : times){
+
+            int start = time[0], leave = time[1];
+
+            while(!leaveChair.isEmpty() && leaveChair.peek()[0]<= start)
+                availableChairs.offer( leaveChair.poll()[1]);
+            
+            int chair;
+            if(!availableChairs.isEmpty())
+                chair = availableChairs.poll();
+            else{
+                chair = currChair;
+                currChair++;
+            }
+
+            leaveChair.add(new int[]{leave, chair});
+
+            if(targetStart == start)
+                return chair;
+        }
+        return -1;
+    }
+}
