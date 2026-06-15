a48c9376d860fec69e4e8511112b32df41e783c6
reach2nimit <reach2nimit@gmail.com>
2026-06-15 12:48:23 +0530

    Time: 6 ms (80.28%), Space: 47.3 MB (83.1%) - LeetHub

---
+++ 0774-minimize-max-distance-to-gas-station/0774-minimize-max-distance-to-gas-station.java
@@ -0,0 +1,27 @@
+class Solution {
+    public double minmaxGasDist(int[] stations, int k) {
+
+        double left = 0, right = 0;
+        for(int i=1; i<stations.length; i++){
+            right = Math.max(right, stations[i] - stations[i-1]);
+        }
+
+        while(right - left > 1e-6){
+            double mid = left + (right - left)/2.0;
+            if(isFeasible(stations, k, mid))
+                right = mid;
+            else
+                left = mid;
+        }
+        return left;
+        
+    }
+
+    public boolean isFeasible(int[] stations, int k, double mid){
+        int count = 0;
+        for(int i = 1 ; i < stations.length; i++)
+            count+= (int)((stations[i] - stations[i-1])/mid);
+        
+        return count<=k;
+    }
+}
